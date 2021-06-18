4a7a7f49000a7bd2674cd7f31f6b2a8122f32b75
ProgrammerJayed <60888930+JayedHasan232@users.noreply.github.com>
2021-06-18 19:55:40 +0600

    SI

--- resources/views/livewire/app/page/products/show.blade.php
+++ resources/views/livewire/app/page/products/show.blade.php
@@ -1,22 +1,13 @@
-<section class="section productShow">
+<section class="productShow py-5">
     <div class="{{ env('BS_CONTAINER') }}">
         <div class="header">
-            <div class="images">
-                <div class="image-gallery">
-                    @for($i = 1; $i <= 5; $i++)
-                    <div class="image-wrapper">
-                        <img src="{{ asset('storage/' . $product->image_medium) }}" alt="{{ __( $product->title ) }}" title="{{ __( $product->title ) }}">
-                    </div>
-                    @endfor
-                </div>
-                <div class="featured-image">
-                    <div class="image-wrapper">
-                        <img src="{{ asset('storage/' . $product->image_medium) }}" alt="{{ __( $product->title ) }}" title="{{ __( $product->title ) }}">
-                    </div>
+            <div class="image">
+                <div class="image-wrapper">
+                    <img src="{{ asset('storage/' . $product->image) }}" alt="{{ __( $product->title ) }}" title="{{ __( $product->title ) }}">
                 </div>
             </div>
             <div class="informations">
-                <h1 class="title">{{ __( $product->title ) }}</h1>
+                <h1 class="title fs-4">{{ __( $product->title ) }}</h1>
                 <h3 class="price">{{ __( '৳' . $product->price ) }}</h3>
                 <p class="overview">{{ __( $product->overview ) }}</p>
                 <button wire:click.prevent="addToCart" class="btn bg-accent text-white">
@@ -26,6 +17,64 @@
                     </svg>
                     {{ __('Add to cart') }}
                 </button>
+            </div>
+            <div class="benefits">
+                <div class="item">
+                    <div class="icon">
+                        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-truck" viewBox="0 0 16 16">
+                            <path d="M0 3.5A1.5 1.5 0 0 1 1.5 2h9A1.5 1.5 0 0 1 12 3.5V5h1.02a1.5 1.5 0 0 1 1.17.563l1.481 1.85a1.5 1.5 0 0 1 .329.938V10.5a1.5 1.5 0 0 1-1.5 1.5H14a2 2 0 1 1-4 0H5a2 2 0 1 1-3.998-.085A1.5 1.5 0 0 1 0 10.5v-7zm1.294 7.456A1.999 1.999 0 0 1 4.732 11h5.536a2.01 2.01 0 0 1 .732-.732V3.5a.5.5 0 0 0-.5-.5h-9a.5.5 0 0 0-.5.5v7a.5.5 0 0 0 .294.456zM12 10a2 2 0 0 1 1.732 1h.768a.5.5 0 0 0 .5-.5V8.35a.5.5 0 0 0-.11-.312l-1.48-1.85A.5.5 0 0 0 13.02 6H12v4zm-9 1a1 1 0 1 0 0 2 1 1 0 0 0 0-2zm9 0a1 1 0 1 0 0 2 1 1 0 0 0 0-2z"/>
+                        </svg>
+                    </div>
+                    <div class="info">
+                        <h3 class="title">Fast Shipping</h3>
+                        <h4 class="sub-title">Receive products in amazing time.</h4>
+                    </div>
+                </div>
+                <div class="item">
+                    <div class="icon">
+                        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-bootstrap-reboot" viewBox="0 0 16 16">
+                            <path d="M1.161 8a6.84 6.84 0 1 0 6.842-6.84.58.58 0 1 1 0-1.16 8 8 0 1 1-6.556 3.412l-.663-.577a.58.58 0 0 1 .227-.997l2.52-.69a.58.58 0 0 1 .728.633l-.332 2.592a.58.58 0 0 1-.956.364l-.643-.56A6.812 6.812 0 0 0 1.16 8z"/>
+                            <path d="M6.641 11.671V8.843h1.57l1.498 2.828h1.314L9.377 8.665c.897-.3 1.427-1.106 1.427-2.1 0-1.37-.943-2.246-2.456-2.246H5.5v7.352h1.141zm0-3.75V5.277h1.57c.881 0 1.416.499 1.416 1.32 0 .84-.504 1.324-1.386 1.324h-1.6z"/>
+                        </svg>
+                    </div>
+                    <div class="info">
+                        <h3 class="title">Easy returns</h3>
+                        <h4 class="sub-title">Return policy that let you shop at ease</h4>
+                    </div>
+                </div>
+                <div class="item">
+                    <div class="icon">
+                        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-shield-fill-check" viewBox="0 0 16 16">
+                            <path fill-rule="evenodd" d="M8 0c-.69 0-1.843.265-2.928.56-1.11.3-2.229.655-2.887.87a1.54 1.54 0 0 0-1.044 1.262c-.596 4.477.787 7.795 2.465 9.99a11.777 11.777 0 0 0 2.517 2.453c.386.273.744.482 1.048.625.28.132.581.24.829.24s.548-.108.829-.24a7.159 7.159 0 0 0 1.048-.625 11.775 11.775 0 0 0 2.517-2.453c1.678-2.195 3.061-5.513 2.465-9.99a1.541 1.541 0 0 0-1.044-1.263 62.467 62.467 0 0 0-2.887-.87C9.843.266 8.69 0 8 0zm2.146 5.146a.5.5 0 0 1 .708.708l-3 3a.5.5 0 0 1-.708 0l-1.5-1.5a.5.5 0 1 1 .708-.708L7.5 7.793l2.646-2.647z"/>
+                        </svg>
+                    </div>
+                    <div class="info">
+                        <h3 class="title">Always authentic product</h3>
+                        <h4 class="sub-title">We only sell 100% authentic products</h4>
+                    </div>
+                </div>
+                <div class="item">
+                    <div class="icon">
+                        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-lock-fill" viewBox="0 0 16 16">
+                            <path d="M8 1a2 2 0 0 1 2 2v4H6V3a2 2 0 0 1 2-2zm3 6V3a3 3 0 0 0-6 0v4a2 2 0 0 0-2 2v5a2 2 0 0 0 2 2h6a2 2 0 0 0 2-2V9a2 2 0 0 0-2-2z"/>
+                        </svg>
+                    </div>
+                    <div class="info">
+                        <h3 class="title">Secure shopping</h3>
+                        <h4 class="sub-title">Your data is always protected</h4>
+                    </div>
+                </div>
+                <div class="item">
+                    <div class="icon">
+                        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-percent" viewBox="0 0 16 16">
+                            <path d="M13.442 2.558a.625.625 0 0 1 0 .884l-10 10a.625.625 0 1 1-.884-.884l10-10a.625.625 0 0 1 .884 0zM4.5 6a1.5 1.5 0 1 1 0-3 1.5 1.5 0 0 1 0 3zm0 1a2.5 2.5 0 1 0 0-5 2.5 2.5 0 0 0 0 5zm7 6a1.5 1.5 0 1 1 0-3 1.5 1.5 0 0 1 0 3zm0 1a2.5 2.5 0 1 0 0-5 2.5 2.5 0 0 0 0 5z"/>
+                        </svg>
+                    </div>
+                    <div class="info">
+                        <h3 class="title">Discount</h3>
+                        <h4 class="sub-title">Get 5% discount</h4>
+                    </div>
+                </div>
             </div>
         </div>
     </div>
